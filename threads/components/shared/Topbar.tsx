c8c729a2911924cde6a58eae7e8c1a146cd2bf31
Motaz_Y <motazyasser84@gmail.com>
2024-07-21 19:17:35 +0300

    HomePage fitst edition topBar and leftSideBar

---
+++ threads/components/shared/Topbar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  ClerkProvider,
+  OrganizationSwitcher,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+export default function Topbar() {
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </div>
+      </div>
+      <OrganizationSwitcher
+        appearance={{
+          elements: {
+            OrganizationSwitcherTrigger: "py-2 px-4",
+          },
+        }}
+      />
+    </nav>
+  );
+}
